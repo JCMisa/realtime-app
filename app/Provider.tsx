b70e697d535dd100660b44c3c62dd1f9b541663f
JC MISA <johncarlomisa399@gmail.com>
2024-07-24 10:45:22 +0800

    implement the mention functionality to mention collaborators in the same document

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,20 +1,34 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import Loader from "@/components/Loader";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
-import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
-      // resolveUsers={async ({ userIds }) => {
-      //   const users = await getClerkUsers();
-      // }}
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
+      }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
